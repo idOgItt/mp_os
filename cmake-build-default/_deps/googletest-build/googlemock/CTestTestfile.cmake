f8c0766a5b7209ee2de860efb1c7c01c56a006e7
stardust <sergeylkd@bk.ru>
2024-05-04 00:15:26 +0300

    Update

--- cmake-build-default/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stardust/Work/University/MAI_Math_Practicum_2/mp_os/cmake-build-default/_deps/googletest-src/googlemock
-# Build directory: /home/stardust/Work/University/MAI_Math_Practicum_2/mp_os/cmake-build-default/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
